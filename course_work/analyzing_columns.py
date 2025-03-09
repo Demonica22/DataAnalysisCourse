6fccac0b32b95b97c14c212f258546ad810ff909
Demonica22 <demonica2003@mail.ru>
2025-03-09 21:36:06 +0300

    Курсовая. Конец

--- course_work/analyzing_columns.py
+++ course_work/analyzing_columns.py
@@ -5,32 +5,35 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('online_gaming_behavior_dataset.csv')
 df.drop('PlayerID', axis=1, inplace=True)
-# columns_to_analyze = ['Age', 'PlayTimeHours', 'SessionsPerWeek', 'AvgSessionDurationMinutes', 'PlayerLevel',
-#                       'AchievementsUnlocked']
+columns_to_analyze = ['Age', 'PlayTimeHours', 'SessionsPerWeek', 'AvgSessionDurationMinutes', 'PlayerLevel',
+                      'AchievementsUnlocked']
+plt.subplots(1, 1)
+for i, column in enumerate(columns_to_analyze):
+    plt.subplot(1, 1, 1)
+    sns.boxplot(df[column])
+plt.tight_layout()
+
+
+plt.show()
+plt.cla()
+
+# columns_to_analyze = ['Gender', 'GameGenre', 'InGamePurchases']
+#
 # plt.subplots(1, len(columns_to_analyze))
 # for i, column in enumerate(columns_to_analyze):
 #     plt.subplot(1, len(columns_to_analyze), i + 1)
-#     sns.boxplot(df[column])
+#     df[column].value_counts().plot.pie(autopct='%1.1f%%',
+#                                        startangle=90,
+#                                        explode=[0.05] * df[column].nunique()
+#                                        )
+#     plt.title(f'{column}')
+#     plt.ylabel('')
 # plt.tight_layout()
 # plt.show()
-
-columns_to_analyze = ['Gender', 'GameGenre', 'InGamePurchases']
-
-plt.subplots(1, len(columns_to_analyze))
-for i, column in enumerate(columns_to_analyze):
-    plt.subplot(1, len(columns_to_analyze), i + 1)
-    df[column].value_counts().plot.pie(autopct='%1.1f%%',
-                                       startangle=90,
-                                       explode=[0.05] * df[column].nunique()
-                                       )
-    plt.title(f'{column}')
-    plt.ylabel('')
-plt.tight_layout()
-plt.show()
-
-numerical_columns = df.select_dtypes(include=['int64', 'float64'])
-correlation_matrix = numerical_columns.corr()
-
-sns.heatmap(correlation_matrix, annot=True, fmt=".2f", linewidths=0.5)
-plt.title("Корреляционная матрица")
-plt.show()
+#
+# numerical_columns = df.select_dtypes(include=['int64', 'float64'])
+# correlation_matrix = numerical_columns.corr()
+#
+# sns.heatmap(correlation_matrix, annot=True, fmt=".2f", linewidths=0.5)
+# plt.title("Корреляционная матрица")
+# plt.show()
--- course_work/part4.py
+++ course_work/part4.py
@@ -5,24 +5,70 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import *
+import scikitplot as skplt
 
 df = pd.read_csv('ready_data.csv')
 
 Y = df['EngagementLevel']
 X = df.drop('EngagementLevel', axis=1)
 scaler = StandardScaler()
-print(X)
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.8, random_state=42, stratify=Y)
-# model = LogisticRegression().fit(X_train, y_train)
-# Y_pred = model.predict(X_test)
-# accuracy = accuracy_score(y_test, Y_pred)
-# print(accuracy)
-for i in range(10, 300, 10):
-    clf = RandomForestClassifier(n_estimators=20).fit(X_train, y_train)
-    predicted = clf.predict(X_test)
-    print(i)
-    print(accuracy_score(y_test, predicted))
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=4, stratify=Y)
+model = LogisticRegression().fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# model = RandomForestClassifier(n_estimators=20, max_depth=13).fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# model = DecisionTreeClassifier(max_depth=11).fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt="d")
+print(classification_report(y_test, y_pred))
+plt.title(f"Confusion Matrix")
+plt.xlabel("Predicted ")
+plt.ylabel("True")
+plt.show()
+roc_auc = roc_auc_score(y_test == 0, y_pred == 0)
+print(f"{roc_auc=}")
+fpr, tpr, _ = roc_curve(y_test == 0, y_pred == 0)
+plt.plot(fpr, tpr)
+plt.xlabel("FalsePositiveRate")
+plt.ylabel("TruePositiveRate")
+plt.title(f"ROC кривая для параметра 0")
+plt.show()
+"""
+13
+0.897464718371425
+train accuracy  0.9675243567324506
+При глубине деревьев в лесу 13 
+
+Параметры max_leaf_nodes и min_samples_leaf заметных изменений метрик не дали.
+"""
+# total_accuracy = accuracy_score(y_test, y_pred)
+# print(f"{total_accuracy=}")
+# y_pred_proba = model.predict_proba(X_test)
+# plt.subplot()
+#
+# for i in [0, 1, 2]:
+#     print(f"Binary param: {i}")
+#     y_pred_bin = y_pred == i
+#     y_test_bin = y_test == i
+#     accuracy = accuracy_score(y_test_bin, y_pred_bin)
+#     precision = precision_score(y_test_bin, y_pred_bin)
+#     recall = recall_score(y_test_bin, y_pred_bin)
+#     print(f"{accuracy=}, {precision=}, {recall=}")
+#     roc_auc = roc_auc_score(y_test_bin, y_pred_bin)
+#     print(f"{roc_auc=}")
+#     fpr, tpr, _ = roc_curve(y_test_bin, y_pred_bin)
+#     plt.plot(fpr, tpr,label=f"{i}" )
+#     # skplt.metrics.plot_roc_curve(y_test_bin, y_pred_proba)
+# plt.figlegend()
+#
+# plt.xlabel("FalsePositiveRate")
+# plt.ylabel("TruePositiveRate")
+# plt.title(f"ROC кривая")
+# plt.show()
